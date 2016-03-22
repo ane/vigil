26875eb4d8ddfd8e894447301209ebc3e0579088
Antoine Kalmbach <antoine.kalmbach@qvantel.com>
2016-03-22 16:22:17 +0200

    In the beginning there was darkness

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to vigil
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject vigil "0.1.0-SNAPSHOT"
+  :description "Watch files as continuous streams"
+  :url "http://github.com/ane/vigil"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [manifold "0.1.3"]
+                 [panoptic "0.2.1"]
+                 [hawk "0.2.10"]])
