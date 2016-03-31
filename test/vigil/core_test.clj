f29d00ab08666fd399dc6f777d47c10d41cee8de
Antoine Kalmbach <ane@iki.fi>
2016-03-31 23:16:12 +0300

    Fix tests, bump coverage to full.

--- test/vigil/core_test.clj
+++ test/vigil/core_test.clj
@@ -1,8 +1,10 @@
 (ns vigil.core-test
-  (:require [clojure.java.io :as io :refer [as-file]]
-            [clojure.test :refer :all]
+  (:require [clojure
+             [string :refer [split-lines]]
+             [test :refer :all]]
+            [manifold.stream :as s]
             [vigil.core :as v]
-            [manifold.stream :as s]))
+            [clojure.java.io :as io]))
 
 (def test-file (atom ""))
 
@@ -55,5 +57,21 @@
       (append-test-content another-str)
       (let [again (v/read-update s file newpos freq)]
         (is (= (+ newpos (.length another-str)) again))))))
+
+(deftest actual-reading
+  (let [test-content "haha\nhehe\nhahah"]
+    (with-open [writer (io/writer @test-file :append true)]
+      (.write writer test-content)
+      (.flush writer)
+      (let [interval 100
+            watcher (v/watch-file @test-file true 100 interval)]
+        (is (= (split-lines test-content) @(s/take! watcher)))
+        (Thread/sleep (inc interval))       ; let the thing rest
+        (doseq [s '("this" "is" "test" "content")]
+          (.write writer s)
+          (.flush writer)
+          (is (= (conj nil s) @(s/take! watcher))))
+        (s/close! watcher)))))
+
 
 (use-fixtures :each create-temp-file)
